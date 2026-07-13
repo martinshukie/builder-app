bc19e2eb63c23b5ac8490cda59939174095161fe
martinshukie <martinshukie69@gmail.com>
2026-07-13 17:53:33 +1000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function App() {
+return (
+<div
+style={{
+fontFamily: "sans-serif",
+padding: "16px",
+maxWidth: "600px",
+margin: "0 auto"
+}}
+>
+<h1>Builder App</h1>
+<p>This app is installable and works offline once loaded.</p>
+<p>Add it to your home screen from the browser menu to use it like a normal app.</p>
+</div>
+);
+}
+
+export default App;
